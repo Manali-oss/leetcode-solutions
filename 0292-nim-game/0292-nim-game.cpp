7d3eb96f059a27ce820bc431da223152a04b066d
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-20 11:23:09 +0530

    Time: 0 ms (100%), Space: 7.7 MB (50.79%) - LeetHub

--- 0292-nim-game/0292-nim-game.cpp
+++ 0292-nim-game/0292-nim-game.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     bool canWinNim(int n) {
         if(n<=3)return true;
-        return n % 4 != 0;
+        if(n%4==0)
+        {
+            return false;
+        }
+        return true;
     }
 };
