a4742f995dcbcb9a6481df8b367b62fe262d7289
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-21 09:56:04 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int zero=0,one=0,two=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                zero++;
+            }
+            else if(nums[i]==1)
+            {
+                one++;
+            }
+            else if(nums[i]==2)
+            {
+                two++;
+            }
+        }
+        int i=0;
+        while(zero--)
+        {
+            nums[i++]=0;
+        }
+        while(one--)
+        {
+            nums[i++]=1;
+        }
+        while(two--)
+        {
+            nums[i++]=2;
+        }
+
     }
 };
