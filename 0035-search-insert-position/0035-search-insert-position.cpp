732a37ba2615f48e7b89ae27e31b07d2219e3732
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-24 11:43:45 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.44%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n=nums.size();
-        for(int i=0;i<n;i++)
+        int start=0,end=n-1;
+        int ans=n;
+        while(start<=end)
         {
-            if(nums[i]==target)
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
             {
-                return i;
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>target)
-            {
-                return i;
-            }
-        }
-        return n;
+        return ans;
     }
 };
