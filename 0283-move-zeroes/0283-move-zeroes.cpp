ae05473c126886f8f9a36be0ca9d2eb640700147
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-25 10:29:20 +0530

    Time: 0 ms (100%), Space: 24 MB (18.92%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>v;
-        int count=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=0)
-            {
-                v.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i<v.size())
-            {
-                nums[i]=v[i];
-            }
-            else
-            {
-                nums[i]=0;
+        int j = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
             }
         }
     }
