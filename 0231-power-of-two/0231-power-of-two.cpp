203ea03b79b6b1f299c2574f2a8d85c28591ba5c
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-26 14:04:14 +0530

    Time: 0 ms (100%), Space: 8 MB (11.83%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==1)return true;
-        long long power=2;
-        while(power<=n)
+        if(n<=0)return 0;
+        while(n>1)
         {
-            if(power==n)return true;
-            power*=2;
+            if(n%2!=0)
+            {
+                return 0;
+            }
+            n/=2;
         }
-        return false;
+        return 1;
     }
 };
