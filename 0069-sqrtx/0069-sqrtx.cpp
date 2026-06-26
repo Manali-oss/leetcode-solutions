9b209b30de66a3fc67058e17bbc5b76cba261331
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-26 14:47:49 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.26%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        int start=0,end=x;
         int ans=0;
-        for(int i=0;i<=x;i++)
+        while(start<=end)
         {
-            if(1LL * i*i<=x)
+            int mid=start+(end-start)/2;
+            long long sqrt=1LL*mid*mid;
+            if(x==sqrt)
             {
-                ans=i;
+                return mid;
+            }
+            else if(sqrt>x)
+            {
+                end=mid-1;
             }
             else
             {
-                break;
+                ans=mid;
+                start=mid+1;
             }
         }
         return ans;
