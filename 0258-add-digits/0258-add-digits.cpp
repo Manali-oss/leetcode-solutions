6a138fd12d8942a248c5dfc6676901b98934cc53
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-26 13:15:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (79.44%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num<10)return num;
-        int sum=0;
-        while(num>9)
-        {
-            sum=0;
-            while(num)
-          {
-            int rem=num%10;
-            sum+=rem;
-            num/=10;
-          }
-          num=sum;
-        }
-        return sum;
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
     }
 };
