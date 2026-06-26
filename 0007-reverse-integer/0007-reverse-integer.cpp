0b01b6991a8b9e36c619b105282622b985a65efb
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-26 13:31:35 +0530

    Time: 3 ms (29.27%), Space: 8.6 MB (20.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int reverse(int x) {
-        int ans=0;
-        while(x!=0){
-            
-           int rem=x%10;
-            x=x/10;
-            if(ans>INT_MAX/10 || ans<INT_MIN/10){
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check for overflow
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
                 return 0;
-            }
-            
-            ans=ans*10+rem;
-         }
-        return ans;
+
+            // Check for underflow
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
+                return 0;
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
     }
 };
