f658668fe50e1134d07857523903cda0871d81bb
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-29 11:38:46 +0530

    Time: 0 ms (100%), Space: 63.5 MB (78.67%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int start=0,end=arr.size()-1;
-        while(start<end)
+        int ans=0;
+        while(start<=end)
         {
             int mid=start+(end-start)/2;
-            if(arr[mid]<arr[mid+1])
+            if(arr[mid]>arr[mid+1])
             {
-                start=mid+1;
+                ans=mid;
+                end=mid-1;
             }
             else
             {
-                end=mid;
+                start=mid+1;
             }
         }
-        return start;
+        return ans;
     }
 };
