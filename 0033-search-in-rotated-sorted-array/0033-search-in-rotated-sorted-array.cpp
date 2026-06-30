abba4442b04126475ad45dcba98df1e755560b7f
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-30 07:53:02 +0530

    Time: 0 ms (100%), Space: 15.2 MB (67.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int start=0,end=nums.size()-1;
-        int ans=-1;
         while(start<=end)
         {
             int mid=start+(end-start)/2;
@@ -33,6 +32,6 @@ public:
                 }
             }
         }
-        return ans;
+        return -1;
     }
 };
