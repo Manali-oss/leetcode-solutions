b1f38b860b951e2b66404a58ac1bc64b20c5f8a5
Manali Dighe <manalirahuldighe@gmail.com>
2026-07-14 19:38:40 +0530

    Time: 10 ms (81.55%), Space: 16 MB (62.27%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int> up(26, 0);
-        vector<int> low(26, 0);
 
-        // Count frequencies
+        vector<int> freq(128, 0);
+        string vowels = "AEIOUaeiou";
+
+        // Count vowels
         for (int i = 0; i < s.size(); i++)
         {
-            if (s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+            if (vowels.find(s[i]) != string::npos)
             {
-                up[s[i]-'A']++;
-            }
-            if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
-            {
-                low[s[i]-'a']++;
+                freq[s[i]]++;
             }
         }
 
-        // Order of vowels
-        vector<int> pos = {0, 4, 8, 14, 20};
+        int j = 0;
 
-        int u = 0, l = 0;
-
-        // Replace vowels
+        // Replace vowels in sorted order
         for (int i = 0; i < s.size(); i++)
         {
-            if (s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||
-                s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            if (vowels.find(s[i]) != string::npos)
             {
-                // Find the smallest available uppercase vowel
-                while (u < 5 && up[pos[u]] == 0)
-                    u++;
-
-                if (u < 5)
+                while (freq[vowels[j]] == 0)
                 {
-                    s[i] = char(pos[u] + 'A');
-                    up[pos[u]]--;
+                    j++;
                 }
-                else
-                {
-                    // All uppercase vowels are used, now use lowercase
-                    while (l < 5 && low[pos[l]] == 0)
-                        l++;
 
-                    s[i] = char(pos[l] + 'a');
-                    low[pos[l]]--;
-                }
+                s[i] = vowels[j];
+                freq[vowels[j]]--;
             }
         }
 
