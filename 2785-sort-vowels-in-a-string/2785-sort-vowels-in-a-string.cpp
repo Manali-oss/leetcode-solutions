cbcfea64f56f26ce0b8bd2f675ca77b5748d4c37
Manali Dighe <manalirahuldighe@gmail.com>
2026-07-14 14:10:48 +0530

    Time: 18 ms (41.51%), Space: 15.4 MB (96.27%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        string temp="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            {
+                temp+=s[i];
+            }
+        }
+        sort(temp.begin(),temp.end());
+        int k=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+            {
+                s[i]=temp[k];
+                k++;
+            }
+        }
+        return s;
+    }
+};
