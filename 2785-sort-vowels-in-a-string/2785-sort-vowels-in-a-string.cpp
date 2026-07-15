b717311b4d434d95d4db8b75f66ce29f282b3de7
Manali Dighe <manalirahuldighe@gmail.com>
2026-07-15 08:42:21 +0530

    Time: 11 ms (78.92%), Space: 15 MB (98.34%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,39 +1,62 @@
 class Solution {
 public:
-    bool isVowel(char ch)
-    {
-        return ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' ||
-               ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
-    }
-
     string sortVowels(string s) {
+        vector<int> up(26, 0);
+        vector<int> low(26, 0);
 
-        vector<int> freq(128, 0);
-        string vowels = "AEIOUaeiou";
-
-        // Count frequencies of vowels
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (isVowel(s[i]))
-            {
-                freq[s[i]]++;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+                up[s[i] - 'A']++;
+            }
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                low[s[i] - 'a']++;
             }
         }
 
-        int j = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' ||
+                s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
 
-        // Replace vowels in sorted order
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (isVowel(s[i]))
-            {
-                while (freq[vowels[j]] == 0)
-                {
-                    j++;
+                if (up['A' - 'A'] > 0) {
+                    s[i] = 'A';
+                    up['A' - 'A']--;
                 }
-
-                s[i] = vowels[j];
-                freq[vowels[j]]--;
+                else if (up['E' - 'A'] > 0) {
+                    s[i] = 'E';
+                    up['E' - 'A']--;
+                }
+                else if (up['I' - 'A'] > 0) {
+                    s[i] = 'I';
+                    up['I' - 'A']--;
+                }
+                else if (up['O' - 'A'] > 0) {
+                    s[i] = 'O';
+                    up['O' - 'A']--;
+                }
+                else if (up['U' - 'A'] > 0) {
+                    s[i] = 'U';
+                    up['U' - 'A']--;
+                }
+                else if (low['a' - 'a'] > 0) {
+                    s[i] = 'a';
+                    low['a' - 'a']--;
+                }
+                else if (low['e' - 'a'] > 0) {
+                    s[i] = 'e';
+                    low['e' - 'a']--;
+                }
+                else if (low['i' - 'a'] > 0) {
+                    s[i] = 'i';
+                    low['i' - 'a']--;
+                }
+                else if (low['o' - 'a'] > 0) {
+                    s[i] = 'o';
+                    low['o' - 'a']--;
+                }
+                else if (low['u' - 'a'] > 0) {
+                    s[i] = 'u';
+                    low['u' - 'a']--;
+                }
             }
         }
 
