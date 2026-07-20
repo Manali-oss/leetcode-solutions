3d535c0ee38e8cd83cba4f110299ee37246ad7b5
Manali Dighe <manalirahuldighe@gmail.com>
2026-07-20 13:23:19 +0530

    Time: 47 ms (74.3%), Space: 53.9 MB (67.9%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -4,30 +4,12 @@
  * @return {Function}
  */
 var timeLimit = function(fn, t) {
-
     return async function(...args) {
-
-        return new Promise((resolve, reject) => {
-
-            const timer = setTimeout(() => {
-                reject("Time Limit Exceeded");
-            }, t);
-
-            fn(...args)
-                .then((res) => {
-                    clearTimeout(timer);
-                    resolve(res);
-                })
-                .catch((err) => {
-                    clearTimeout(timer);
-                    reject(err);
-                });
-
-        });
+        return Promise.race([
+            fn(...args),
+            new Promise((_, reject) =>
+                setTimeout(() => reject("Time Limit Exceeded"), t)
+            )
+        ]);
     };
 };
-
-/**
- * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
- * limited(150).catch(console.log) // "Time Limit Exceeded"
- */
